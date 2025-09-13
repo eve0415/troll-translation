9cd6f0f912706a8b47c3168887e75ca94f86f88f
eve0415 <40632436+eve0415@users.noreply.github.com>
2025-09-13 10:38:26 +0900

    refactor: lint code

--- src/router.tsx
+++ src/router.tsx
@@ -1,20 +1,21 @@
-import { createRouter as createTanStackRouter } from "@tanstack/react-router";
-import { routeTree } from "./routeTree.gen";
+import { createRouter as createTanStackRouter } from '@tanstack/react-router';
+
+import { routeTree } from './routeTree.gen';
 
 export function createRouter() {
-	if (import.meta.env.DEV && typeof window !== "undefined") {
-		// @ts-expect-error Virtual module
-		import("virtual:browser-echo").catch(console.error);
-	}
+  if (import.meta.env.DEV && typeof window !== 'undefined') {
+    // @ts-expect-error Virtual module
+    import('virtual:browser-echo').catch(console.error);
+  }
 
-	return createTanStackRouter({
-		routeTree,
-		scrollRestoration: true,
-	});
+  return createTanStackRouter({
+    routeTree,
+    scrollRestoration: true,
+  });
 }
 
-declare module "@tanstack/react-router" {
-	interface Register {
-		router: ReturnType<typeof createRouter>;
-	}
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: ReturnType<typeof createRouter>;
+  }
 }
